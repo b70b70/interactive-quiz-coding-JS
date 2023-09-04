1d7482ff36f93e9e77028a25c1149b25c7d6d520
Ali <134099889+b70b70@users.noreply.github.com>
2023-09-05 00:27:07 +0100

    created files for the scripts needed. added scripts to html fils and begun the highscores script.

---
+++ assets/js/scores.js
@@ -0,0 +1,12 @@
+function printHighScores(){
+ //TODO
+}
+
+function clearHighScores(){
+//TODO
+}
+
+let clearButton = document.getAnimations("clear");
+clearButton.addEventListener("click", clearHighScores);
+
+printHighScores();
