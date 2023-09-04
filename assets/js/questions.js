0d848b00fb0b5d89a95d559557cdafc2fe0670a1
Ali <134099889+b70b70@users.noreply.github.com>
2023-09-05 00:36:50 +0100

    created set of questions with choices and the answer for the questions script

---
+++ assets/js/questions.js
@@ -0,0 +1,19 @@
+let questions = [
+    {
+        "title": "Which of the following DOES NOT include Common Data types",
+        "choices": ["string", "booleans", "alerts", "numbers"],
+        "answer": "alerts"
+
+    },
+    {
+        "title": "Which operator is used to combine two or more strings in JavaScript?",
+        "choices": ["+", "-", "*", "/"],
+        "answer": "+"
+    },
+    {
+        "title": "What does the DOM stand for in JavaScript",
+        "choices": ["Document Object Model", "Dynamic Object Manipulation", "Data Object Model", "for  Document Orientation Modeleach"],
+        "answer": "Document Object Model"
+    }
+    
+]
