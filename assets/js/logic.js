17e70cd8c45b68b24bb7a3414bc3b298c761ed50
Ali <134099889+b70b70@users.noreply.github.com>
2023-09-05 01:05:32 +0100

    added function so when user clicks interface to start quiz, the page is cleared ready to populate with questions and timer starts upon user starting quiz and quiz finishes when timer goes to 0

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -24,21 +24,37 @@
 
     }
 
-
-    function startQuiz(){
-        alert("hi");
-    }
-
-
+    
     function quizEnd(){
+        clearInterval(timerID);
 
+        let endScreenElement = document.getElementById("end-screen");
+        endScreenElement.removeAttribute("class");
+
+        let finalScoreElement = document.getElementById("final-score")
+        finalScoreElement.textContent = time;
+
+        questionsElement.setAttribute("class", "hide");
     }
-
 
     function clockTick(){
+        time--;
+        timerElement.textContent = time;
 
+        if(time <= 0){
+            quizEnd();
+        }
     }
 
+    function startQuiz(){
+        let startScreenElement = document.getElementById("start-screen");
+        startScreenElement.setAttribute("class", "hide");
+
+        questionsElement.removeAttribute("class");
+
+        timerID = setInterval(clockTick, 1000)
+        
+    }
 
     function saveHighScore(){
 
@@ -50,3 +66,7 @@
     }
 
     startButton.addEventListener("click", startQuiz);
+    submitButton.addEventListener("click", saveHighScore);
+    initialElement.addEventListener("keyup", checkForEnter)
+
+    
