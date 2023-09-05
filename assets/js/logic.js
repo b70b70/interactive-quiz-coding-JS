2df1bb5db266774fdf86a98528d77c816f69ad6d
Ali <134099889+b70b70@users.noreply.github.com>
2023-09-05 01:20:50 +0100

    completed function getQuestion, this now populates the page with the questions when user starts the quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,12 +10,32 @@
     let choicesElement = document.getElementById("choices");
     let submitButton = document.getElementById("submit");
     let startButton = document.getElementById("start");
-    let initialElement = document.getElementById("initials")
-    let feedbackElement = document.getElementById ("feedback")
+    let initialElement = document.getElementById("initials");
+    let feedbackElement = document.getElementById ("feedback");
 
-    let sfx = new Audio("assets/sfx/correct.wav")
+    let sfx = new Audio("assets/sfx/correct.wav");
 
     function getQuestion(){
+        let currentQuestion = questions[currentQuestionIndex];
+        
+        let titleElement = document.getElementById("question-title");
+
+        titleElement.textContent = currentQuestion.title;
+
+        choicesElement.innerHTML = "";
+
+        currentQuestion.choices.forEach(function(choice, index){
+            let choiceButton = document.createElement("button");
+
+            choiceButton.setAttribute("class", "choice");
+            choiceButton.setAttribute("value",  choice);
+            
+            choiceButton.textContent = `${index+1}. ${choice}`
+
+            choiceButton.addEventListener("click", questionClick);
+
+            choicesElement.append(choiceButton);
+        })
 
     }
     
@@ -53,7 +73,11 @@
         questionsElement.removeAttribute("class");
 
         timerID = setInterval(clockTick, 1000)
-        
+
+        timerElement.textContent = time;
+
+        getQuestion();
+
     }
 
     function saveHighScore(){
